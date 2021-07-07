4754553ff94ca8a1558510b3c5e5fe29898ea5c3
ArgusSanguine <argusandaurus@gmail.com>
2021-07-07 17:27:46 -0500

    Removed connection string

--- StoreDL/Entities/DemoDbContext.cs
+++ StoreDL/Entities/DemoDbContext.cs
@@ -21,17 +21,7 @@ namespace StoreDL.Entities
         public virtual DbSet<LineItem> LineItems { get; set; }
         public virtual DbSet<Order> Orders { get; set; }
         public virtual DbSet<Product> Products { get; set; }
-        public virtual DbSet<Restaurant> Restaurants { get; set; }
         public virtual DbSet<StoreFront> StoreFronts { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=tcp:silverserver.database.windows.net,1433;Initial Catalog=DemoDb;Persist Security Info=False;User ID=TheArgentus;Password=argusSanguine518^;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-            }
-        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -118,27 +108,6 @@ namespace StoreDL.Entities
                     .IsUnicode(false);
 
                 entity.Property(e => e.Price).HasColumnType("money");
-            });
-
-            modelBuilder.Entity<Restaurant>(entity =>
-            {
-                entity.HasNoKey();
-
-                entity.ToTable("Restaurant");
-
-                entity.Property(e => e.City)
-                    .HasMaxLength(30)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.Id).ValueGeneratedOnAdd();
-
-                entity.Property(e => e.RestaurantName)
-                    .HasMaxLength(40)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.RestaurantState)
-                    .HasMaxLength(30)
-                    .IsUnicode(false);
             });
 
             modelBuilder.Entity<StoreFront>(entity =>
