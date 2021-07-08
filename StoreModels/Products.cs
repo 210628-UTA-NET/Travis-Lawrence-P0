784657bacbb9c05c388ceb115e0153684b959838
ArgusSanguine <argusandaurus@gmail.com>
2021-07-08 16:49:40 -0500

    Added ToString() override

--- StoreModels/Products.cs
+++ StoreModels/Products.cs
@@ -6,14 +6,10 @@ namespace StoreModels
         public Products(){
             Name = "";
             Price = 0;
-            Desc = "N/A";
-            Category = "N/A";
         }
         public Products(string p_name, double p_price){
             Name = p_name;
             Price = p_price;
-            Desc = "N/A";
-            Category = "N/A";
         }
         public Products(string p_name, double p_price, string p_desc, string p_cat){
             Name = p_name;
@@ -26,5 +22,18 @@ namespace StoreModels
         public double Price{get; set;}
         public string Desc{get; set;}
         public string Category{get; set;}
+
+
+        public override string ToString()
+        {
+            string retString = $"Product Name: {Name}\nPrice: {Price}";
+            if(Desc != null){
+                String.Concat(retString, $"\nDescription: {Desc}");
+            }
+            if(Category != null){
+                String.Concat(retString, $"\nCategory: {Category}");
+            }
+            return retString;
+        }
     }
 }
