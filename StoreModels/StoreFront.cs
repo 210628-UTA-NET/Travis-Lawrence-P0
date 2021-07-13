2fe7ec49e8a819cd0e3c02b32d0848cff0d290b1
ArgusSanguine <argusandaurus@gmail.com>
2021-07-13 15:05:55 -0500

    Changed Orders to actual property

--- StoreModels/StoreFront.cs
+++ StoreModels/StoreFront.cs
@@ -4,24 +4,24 @@ using System.Collections.Generic;
 namespace StoreModels
 {
     public class StoreFront{
-        private List<Orders> _orders;
 
         public StoreFront(){
             Name = "";
             Address = "";
             Inventory = new List<LineItems>();
-            _orders = new List<Orders>();
+            Orders = new List<Orders>();
         }
         public StoreFront(string p_name, string p_addr, List<LineItems> p_inventory, List<Orders> p_orders){
             Name = p_name;
             Address = p_addr;
             Inventory = p_inventory;
-            _orders = p_orders;
+            Orders = p_orders;
         }
 
+        public int StoreID{get; set;}
         public string Name{get; set;}
         public string Address{get; set;}
-
         public List<LineItems> Inventory{get; set;}
+        public List<Orders> Orders{get; set;}
     }
 }
